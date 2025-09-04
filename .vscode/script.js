e1999e5641b6d829d46d814b71431847c3d1ca08
Gustavo Soares <gustavo.soares.henrique@hotmail.com>
2025-09-04 20:07:44 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "/assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "/assets/avatar.png")
+  }
+
+  const alt = document.documentElement
+
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "alt",
+      "foto do mayke brito de oculos sorrindo com fundo rosa e azul"
+    )
+  } else {
+    img.setAttribute(
+      "alt",
+      "foto do mayke brito sorrindo, com camisa preta e fundo laranja"
+    )
+  }
+}
